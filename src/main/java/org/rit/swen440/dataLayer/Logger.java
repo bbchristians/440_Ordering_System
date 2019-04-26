1b54f709d4fc02abe9c6cb6359ad63095db297a7
Kent Brown <randall.k.brown.jr@gmail.com>
2019-04-26 13:56:30 -0400

    Added additional logging statements.

--- src/main/java/org/rit/swen440/dataLayer/Logger.java
+++ src/main/java/org/rit/swen440/dataLayer/Logger.java
@@ -1,27 +1,30 @@
 package org.rit.swen440.dataLayer;
 
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.util.Date;
 
 public class Logger {
-    private PrintWriter writer;
 
     public static Logger OSLogger = new Logger();
 
-    public Logger(){
-        try {
-            this.writer = new PrintWriter("log.txt", "UTF-8");
-        } catch (Exception e) { }
+    private Logger(){
+        // This class has no state!
     }
 
     public void log(String severity, String message){
-        Date date = new Date();
-        String toLog = date.toString() + " | " + severity + ":\n";
-        toLog += "  " + message;
-        writer.println(toLog);
-    }
-
-    public void closeLog(){
-        writer.close();
+        try {
+            OutputStream os = new FileOutputStream("log.txt", true);
+            PrintWriter pw = new PrintWriter(os);
+            Date date = new Date();
+            String toLog = date.toString() + " | " + severity + ":\n";
+            toLog += "  " + message;
+            pw.println(toLog);
+            pw.close();
+        } catch (Exception e) {
+            System.err.println("Failed to open the log file! Error: " + e.toString());
+            System.exit(0);
+        }
     }
 }
